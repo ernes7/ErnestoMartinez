ef298f85726abb0f919f2763218cf47f1da66993
Ernesto <ernechelsea@gmail.com>
2021-02-23 10:23:03 -0800

    added files for project section and icons for meta data

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProjectsSection.scss'
+
+export default function ProjectsSection() {
+    return (
+        <div className={"container"}>
+            <h2>Projects</h2>
+            <div className={"project"}>...lorem
+            </div>
+        </div>
+    )
+}
